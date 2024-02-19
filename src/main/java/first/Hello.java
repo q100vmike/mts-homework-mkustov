5d8daedf861695f4fafa77cfd49dc3b6e1536cb5
q100vmike <melkin@mail.ru>
2024-02-19 17:29:39 +0300

    commit3

--- src/main/java/first/Hello.java
+++ src/main/java/first/Hello.java
@@ -2,6 +2,6 @@ package first;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        System.out.println("Привет из feature-branch!");
     }
 }
